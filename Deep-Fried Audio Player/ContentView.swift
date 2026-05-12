43d2b414655eff9f8e30c67d02fa948fe851f973
hedssaz <hedssaz@gmail.com>
2026-05-13 01:22:18 +0800

    Initial Commit

---
+++ Deep-Fried Audio Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deep-Fried Audio Player
+//
+//  Created by hedssaz on 2026/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deep-Fried Audio Player/Deep_Fried_Audio_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deep_Fried_Audio_PlayerApp.swift
+//  Deep-Fried Audio Player
+//
+//  Created by hedssaz on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct Deep_Fried_Audio_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
